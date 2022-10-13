87995243ca26fece4fbc139b0b5e3ea8662a9a18
Alexander <alexgriffith360@gmail.com>
2022-10-13 14:37:49 -0400

    repeatString solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  // repeat a string n times without using String.protoype.repeat()
+  if (num < 0) return "Number input error. Input >= 0";
+  let result = "";
+  for (let i = 0; i < num; i++) {
+    result += string;
+  }
+  return result;
 };
 
 // Do not edit below this line
