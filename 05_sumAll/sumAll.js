03f88f79cb4c955ef73c56e2b1295223842d7138
Alexander <alexgriffith360@gmail.com>
2022-10-16 10:17:38 -0400

    sumAll solution and test suite activation

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function (...args) {
+  if (args.some((x) => x < 0)) return "ERROR";
+  if (args.some((x) => typeof x !== "number")) return "ERROR";
+  const [n1, n2] = args.sort((a, b) => a - b);
+  return ((n2 - n1 + 1) * (n2 - n1 + 2)) / 2;
 };
 
 // Do not edit below this line
