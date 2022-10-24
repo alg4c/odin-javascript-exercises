498df56db10d10240332e3e8dfd36adcd1d4e460
Alexander <alexgriffith360@gmail.com>
2022-10-24 13:14:09 -0400

    fibonacci solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function (targetMember) {
+  if (targetMember < 0) return "OOPS";
+  const fib = [1, 1];
+  for (let i = 1; i < targetMember; i++) {
+    fib.push(fib[i] + fib[i - 1]);
+  }
+  return fib[targetMember - 1];
 };
 
 // Do not edit below this line
