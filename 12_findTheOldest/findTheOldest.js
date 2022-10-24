cf5f3359fd0f3a0edc5eb286611e0722520dd0c1
Alexander <alexgriffith360@gmail.com>
2022-10-24 18:36:06 -0400

    findTheOldest solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (personObjArr) {
+  let result = personObjArr[0];
+  for (let person of personObjArr) {
+    person.age =
+      person.yearOfDeath !== undefined
+        ? person.yearOfDeath - person.yearOfBirth
+        : new Date().getFullYear() - person.yearOfBirth;
+    if (person.age > result.age) result = person;
+  }
+  return result;
 };
 
 // Do not edit below this line
