25d957bf51112a449ca3cf34ce6305181107ce0d
Alexander <alexgriffith360@gmail.com>
2022-10-24 12:52:34 -0400

    palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const strArr = string
+    .toLowerCase()
+    .split("")
+    .filter((x) => x.match(/[a-zA-z]/g));
+  const midPointIndex = Math.floor(strArr.length / 2);
 
+  for (let i = 0; i < midPointIndex; i++) {
+    if (strArr[i] != strArr[strArr.length - i - 1]) return false;
+  }
+  return true;
 };
 
+palindromes("racecar!");
 // Do not edit below this line
 module.exports = palindromes;
